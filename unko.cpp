e42b189d1f5093d4d6a886d1e17a8332ec2f8674
Eclipse1024 <96379307+Eclipse1024@users.noreply.github.com>
2021-12-19 22:20:48 +0900

    Add files via upload

---
+++ unko.cpp
@@ -0,0 +1,15 @@
+#include "unko.h"
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+
+unko::unko()
+{
+	#include <string.h>
+	char Str[32];
+	#include <Windows.h>
+	strcpy(Str, "Hello World");
+	Sleep(this->sheep);
+	printf("%s", Str);
+	Sleep(this->sheep);
+}
