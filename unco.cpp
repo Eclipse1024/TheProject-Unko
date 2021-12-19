03d8768f8646567f1394831c425ef9332c7ec0fe
Eclipse1024 <96379307+Eclipse1024@users.noreply.github.com>
2021-12-19 22:21:46 +0900

    Create unco.cpp

---
+++ unco.cpp
@@ -0,0 +1,14 @@
+//--------------------------------
+//Creator:Mr.Unkokko & Kusa.jp
+//Day	 :12/19/2021
+//--------------------------------
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+#include "unko.h"
+
+int main(void)
+{
+	unko unco;
+	return 0;
+}
